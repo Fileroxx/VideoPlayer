c0e1a3a6c0207713ba60ed7c89b73ec9402e0431
Igor Fernandes <igor_cesari@hotmail.com>
2022-01-14 18:04:21 -0300

    Atualizando - VideoPlayer

--- js/script.js
+++ js/script.js
@@ -6,8 +6,18 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const copyText = document.getElementById('copiar')
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+console.log(checkbox)
+
 
 /* BUILD OUT FUNCTIONS */
+
 function togglePlay() {
 
     const method = video.paused ? 'play' : 'pause';
@@ -28,7 +38,7 @@ function toggleMute() {
 function updateButton() {
     const icon = this.paused ? '▶' : 'Pause';
     console.log(icon);
-    toggle.textContent = icon;
+    toggle.textContent = icon; 
 }
 
 function skip() {
@@ -54,6 +64,18 @@ function scrub(e) {
 }
 
 
+function copiarElemento() {
+   
+
+    copyText.select();
+    copyText.setSelectionRage(0, 99999);
+
+    navigator.clipboard.writeText(copyText.value);
+
+    alert("Copied the text:" + copyText.value)
+
+}
+
 
 /* HOOK UP THE EVENT LISTENERS */
 video.addEventListener('click', togglePlay);
@@ -62,12 +84,12 @@ video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 video.addEventListener('click', toggleMute)
 
+copyElement.addEventListener('click', copiarElemento)
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 progress.addEventListener('click', scrub);
-
-
 
