6f9730306312b9e3a9d1d13fadcf3fabd72c8bab
Igor Fernandes <igor_cesari@hotmail.com>
2022-01-18 18:04:20 -0300

    Atualização 1

--- js/script.js
+++ js/script.js
@@ -6,14 +6,28 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
-const copyText = document.getElementById('copiar')
+const copyText = document.getElementById('copiar');
 const checkbox = document.getElementById('checkbox');
+
+const fileVideo = document.getElementById('iframefile').files[0];
+
+
+const produto = document.getElementById('urlProduct');
+const tempo = document.getElementById('tempo');
+
+const result = document.getElementById('result');
+
 
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
 
 console.log(checkbox)
+
+
+const colorInput = document.getElementById('colorPicker')
+// const theColor = colorInput.value;
+
 
 
 /* BUILD OUT FUNCTIONS */
@@ -50,12 +64,20 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
     console.log(this.name); 
     console.log(this.value);
-    
+    bolinhaRange.style.backgroundColor = `${colorInput.value}`
 }
 
 function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
+    progressBar.style.backgroundColor = `${colorInput.value}`
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+
+    video.currentTime = scrubTime;
+    console.log(e);
 }
 
 
@@ -71,5 +93,31 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+let mousedown = false;
+
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false)
+
+colorInput.value(localStorage['setcolor']);
+colorInput.onchange(() => {
+    var colorvalue = this.value;
+    localStorage.setItem('setcolor', colorvalue);
+
+})
+
+
+bolinhaRange.value(localStorage['setcolor']);
+bolinhaRange.onchange(() => {
+    var colorvalue = this.value;
+    localStorage.setItem('setcolor', colorvalue);
+
+})
+
+
+
+
+
+
 
